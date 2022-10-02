102049f18d85b07f0d6b04e86b2a7e190e3c8a11
Андрей Поволоцкий <nol1ez@outlook.com>
2022-10-02 19:12:40 +0300

    save 1,1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from application import salary
 from application.db import people
+from datetime import datetime
 
 if __name__ == '__main__':
+    print(datetime.today())
     print(salary.calculate_salary())
     print(people.get_employees())
 
